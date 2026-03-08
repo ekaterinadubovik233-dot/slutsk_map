7a50bb03d22b3c5fe7ff0a0a12e5031c510ef8bf
ekaterinadubovik233-dot <ekaterinadubovik233@gmail.com>
2026-03-08 22:57:27 +0300

    основа

--- js/map.js
+++ js/map.js
@@ -1,15 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
-    const map = L.map('map', { 
-        attributionControl: false,
-        zoomControl: true 
-    }).setView([53.0276, 27.5597], 14);
-
+    const map = L.map('map', { attributionControl: false, zoomControl: true }).setView([53.0276, 27.5597], 14);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-    const markersGroup = L.markerClusterGroup({
-        showCoverageOnHover: false, disableClusteringAtZoom: 13, spiderfyOnMaxZoom: true
-    });
+    const markersGroup = L.markerClusterGroup({ showCoverageOnHover: false, disableClusteringAtZoom: 13, spiderfyOnMaxZoom: true });
     map.addLayer(markersGroup);
 
     const attractionsData = {
@@ -18,14 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
             { "type": "Feature", "properties": { "ID": 1, "NAME": "Часовня Святой Варвары", "DESCRIPTION": "Часовня Святой Варвары – одна из красивейших архитектурных достопримечательностей города Слуцка. Она находится у моста через реку Случь по улице М. Богдановича и привлекает внимание приезжих гостей и туристов. С часовни Святой Варвары открывается красивый вид на город и на соседний культурно-значимый объект Слуцка – памятник Софии Слуцкой.", "IMAGES_DIR": "images/varvara_chapel", "IMAGES_COUNT": 4, "AUDIO": "audio/varvara_chapel.mp3", "VIDEO": "video/varvara_chapel.mp4", "TYPE": "chapel" }, "geometry": { "type": "Point", "coordinates": [27.55419, 53.02608] } },
             { "type": "Feature", "properties": { "ID": 2, "NAME": "Памятник войнам-освободителям", "DESCRIPTION": "Впечатляющий мемориал изображает советского солдата — израненного молодого мужчину, который рвётся в последний бой. За его спиной развевается красное знамя. Скульптура выполнена из бронзы и помещена на тумбу из красного гранита. По окружности площади установлены доски с именами павших слутчан.", "IMAGES_DIR": "images/monument_soldier", "IMAGES_COUNT": 3, "AUDIO": "audio/monument_soldier.mp3", "VIDEO": "video/monument_soldier.mp4", "TYPE": "monument" }, "geometry": { "type": "Point", "coordinates": [27.55397, 53.02730] } },
             { "type": "Feature", "properties": { "ID": 3, "NAME": "Памятник Софии Слуцкой", "DESCRIPTION": "Памятник Софии Слуцкой был создан скульптором Михаилом Иньковым и архитектором Николаем Лукьянчиком. Памятник представляет собой отлитую из бронзы фигуру святой, а также высокую арку за спиной.", "IMAGES_DIR": "images/monument_sofia", "IMAGES_COUNT": 5, "AUDIO": "audio/monument_sofia.mp3", "VIDEO": "video/monument_sofia.mp4", "TYPE": "monument" }, "geometry": { "type": "Point", "coordinates": [27.55375, 53.02630] } },
-            { "type": "Feature", "properties": { "ID": 4, "NAME": "Макет храма чудотворца", "DESCRIPTION": "Бронзовый макет древнего храма в честь Николая Чудотворца. Он был основан в XII столетии. В 1934 г. храм был взорван.", "IMAGES_DIR": "images/model_of_temple", "IMAGES_COUNT": 4, "AUDIO": "audio/model_of_temple.mp3", "VIDEO": "video/model_of_temple.mp4", "TYPE": "monument" }, "geometry": { "type": "Point", "coordinates": [27.55400, 53.02659] } },
+            { "type": "Feature", "properties": { "ID": 4, "NAME": "Макет храма чудотворца", "DESCRIPTION": "Бронзовый макет древнего храма в честь Николая Чудотворца. Он был основан в XII столетии. В 1898 году его перестроили в камне, в неовизантийском стиле. Несмотря на реконструкцию, в храме сохранилась усыпальница князей Олельковичей и Радзивиллов. В 1934 г. храм был взорван.", "IMAGES_DIR": "images/model_of_temple", "IMAGES_COUNT": 4, "AUDIO": "audio/model_of_temple.mp3", "VIDEO": "video/model_of_temple.mp4", "TYPE": "monument" }, "geometry": { "type": "Point", "coordinates": [27.55400, 53.02659] } },
             { "type": "Feature", "properties": { "ID": 5, "NAME": "Дом Культуры", "DESCRIPTION": "Дом культуры - здание в Слуцке, расположенное в сквере по ул. Большая Горка. Памятник архитектуры неоклассицизма.", "IMAGES_DIR": "images/house_of_culture", "IMAGES_COUNT": 3, "AUDIO": "audio/house_of_culture.mp3", "VIDEO": "video/house_of_culture.mp4", "TYPE": "museum" }, "geometry": { "type": "Point", "coordinates": [27.55563, 53.02807] } },
             { "type": "Feature", "properties": { "ID": 6, "NAME": "Музей Слуцкие пояса", "DESCRIPTION": "Музей истории Слуцких поясов хранит в себе великое национальное наследие и прекрасный образец декоративно-прикладного искусства.", "IMAGES_DIR": "images/museum_of_belts", "IMAGES_COUNT": 5, "AUDIO": "audio/museum_of_belts.mp3", "VIDEO": "video/museum_of_belts.mp4", "TYPE": "museum" }, "geometry": { "type": "Point", "coordinates": [27.55531, 53.02443] } },
             { "type": "Feature", "properties": { "ID": 7, "NAME": "Гимназия №1", "DESCRIPTION": "Основана в 1617 году князем Янушем Радзивиллом, что делает её одним из старейших учебных заведений в Республике.", "IMAGES_DIR": "images/gymnasium", "IMAGES_COUNT": 4, "AUDIO": "audio/gymnasium.mp3", "VIDEO": "video/gymnasium.mp4", "TYPE": "school" }, "geometry": { "type": "Point", "coordinates": [27.55914, 53.02604] } },
             { "type": "Feature", "properties": { "ID": 8, "NAME": "Памятник Анастасии Слуцкой", "DESCRIPTION": "Памятник Анастасии Слуцкой в Слуцке – это дань памяти мужеству и отваге этой хрупкой физически, но крепкой духом девушки.", "IMAGES_DIR": "images/monument_anastasia", "IMAGES_COUNT": 3, "AUDIO": "audio/monument_anastasia.mp3", "VIDEO": "video/monument_anastasia.mp4", "TYPE": "monument" }, "geometry": { "type": "Point", "coordinates": [27.55522, 53.02768] } },
             { "type": "Feature", "properties": { "ID": 9, "NAME": "Краеведческий музей", "DESCRIPTION": "Слуцкий краеведческий музей расположен в Доме бывшего дворянского собрания, построенного в конце XVIII века.", "IMAGES_DIR": "images/local_history_museum", "IMAGES_COUNT": 3, "AUDIO": "audio/local_history_museum.mp3", "VIDEO": "video/local_history_museum.mp4", "TYPE": "museum" }, "geometry": { "type": "Point", "coordinates": [27.54690, 53.02375] } },
             { "type": "Feature", "properties": { "ID": 10, "NAME": "Бывшая почтовая станция", "DESCRIPTION": "Почтовая станция располагалась в великолепном здании начала XIX века. Его украшали белоснежные колонны.", "IMAGES_DIR": "images/post_office", "IMAGES_COUNT": 5, "AUDIO": "audio/post_office.mp3", "VIDEO": "video/post_office.mp4", "TYPE": "museum" }, "geometry": { "type": "Point", "coordinates": [27.54511, 53.02254] } },
-            { "type": "Feature", "properties": { "ID": 11, "NAME": "Духовное училище", "DESCRIPTION": "Духовное училище было построено в XIX веке. В настоящее время здание занимает медицинский колледж.", "IMAGES_DIR": "images/theological_school", "IMAGES_COUNT": 6, "AUDIO": "audio/theological_school.mp3", "VIDEO": "video/theological_school.mp4", "TYPE": "museum" }, "geometry": { "type": "Point", "coordinates": [27.54584, 53.01792] } },
+            { "type": "Feature", "properties": { "ID": 11, "NAME": "Духовное училище", "DESCRIPTION": "Духовное училище было построено в XIX века. В настоящее время здание занимает медицинский колледж.", "IMAGES_DIR": "images/theological_school", "IMAGES_COUNT": 6, "AUDIO": "audio/theological_school.mp3", "VIDEO": "video/theological_school.mp4", "TYPE": "museum" }, "geometry": { "type": "Point", "coordinates": [27.54584, 53.01792] } },
             { "type": "Feature", "properties": { "ID": 12, "NAME": "Собор святого Михаила", "DESCRIPTION": "Уникальный сохранившийся пример Слуцкой школы полесского деревянного церковного зодчества XVIII века.", "IMAGES_DIR": "images/michael_cathedral", "IMAGES_COUNT": 3, "AUDIO": "audio/michael_cathedral.mp3", "VIDEO": "video/michael_cathedral.mp4", "TYPE": "chapel" }, "geometry": { "type": "Point", "coordinates": [27.57840, 53.03068] } }
         ]
     };
@@ -45,11 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
     L.geoJSON(attractionsData, {
         pointToLayer: (f, latlng) => {
             const colorClass = getPinColor(f.properties.TYPE);
-            const icon = L.divIcon({
-                className: 'custom-div-icon',
-                html: `<div class="custom-pin ${colorClass}"><img src="img/icons/${f.properties.TYPE}.png"></div>`,
-                iconSize: [36, 36], iconAnchor: [18, 36]
-            });
+            const icon = L.divIcon({ className: 'custom-div-icon', html: `<div class="custom-pin ${colorClass}"><img src="img/icons/${f.properties.TYPE}.png"></div>`, iconSize: [36, 36], iconAnchor: [18, 36] });
             const m = L.marker(latlng, { icon: icon });
             m.bindTooltip(f.properties.NAME, { direction: 'top', offset: [0, -35] });
             return m;
@@ -110,10 +99,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const lng = start.lng + (end.lng - start.lng) * segmentProgress;
         const pos = [lat, lng];
         if (carMarker) carMarker.setLatLng(pos);
-        if (followCar && time - lastPanTime > 40) {
-            map.panTo(pos, { animate: false });
-            lastPanTime = time;
-        }
+        if (followCar && time - lastPanTime > 40) { map.panTo(pos, { animate: false }); lastPanTime = time; }
         requestAnimationFrame(animate);
     }
 
@@ -154,34 +140,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function filterList(searchValue) {
         const val = searchValue.toLowerCase();
-        document.querySelectorAll('#route-list li').forEach(li => {
-            li.style.display = li.textContent.toLowerCase().includes(val) ? 'flex' : 'none';
-        });
+        document.querySelectorAll('#route-list li').forEach(li => { li.style.display = li.textContent.toLowerCase().includes(val) ? 'flex' : 'none'; });
     }
 
     document.getElementById('pcSearch').oninput = (e) => filterList(e.target.value);
     document.getElementById('mobileSearch').oninput = (e) => filterList(e.target.value);
-    document.getElementById('closeDetailsBtn').onclick = () => {
-        document.getElementById('details-audio').pause();
-        document.getElementById('detailsPanel').classList.remove('active');
-        isWaitingForClose = false;
-    };
+    document.getElementById('closeDetailsBtn').onclick = () => { document.getElementById('details-audio').pause(); document.getElementById('detailsPanel').classList.remove('active'); isWaitingForClose = false; };
     document.getElementById('openVideoBtn').onclick = () => {
         const v = document.getElementById('details-video');
         v.src = currentProps.VIDEO;
         document.getElementById('video-modal-title').textContent = currentProps.NAME;
         document.getElementById('videoModal').style.display = 'block';
     };
-    document.getElementById('closeVideoBtn').onclick = () => {
-        document.getElementById('details-video').pause();
-        document.getElementById('videoModal').style.display = 'none';
-    };
+    document.getElementById('closeVideoBtn').onclick = () => { document.getElementById('details-video').pause(); document.getElementById('videoModal').style.display = 'none'; };
     document.getElementById('toggleSidebar').onclick = () => document.getElementById('sidebar').classList.add('active');
     document.getElementById('closeSidebar').onclick = () => document.getElementById('sidebar').classList.remove('active');
     document.getElementById('closeZoomBtn').onclick = () => document.getElementById('imageModal').style.display = 'none';
     document.getElementById('pauseCar').onclick = () => isPaused = !isPaused;
     document.getElementById('clearRoute').onclick = () => location.reload();
-    document.getElementById('followCar').onclick = (e) => { 
-        followCar = !followCar; e.target.textContent = `🎥 Слежение: ${followCar ? 'ВКЛ' : 'ВЫКЛ'}`; 
-    };
+    document.getElementById('followCar').onclick = (e) => { followCar = !followCar; e.target.textContent = `🎥 Слежение: ${followCar ? 'ВКЛ' : 'ВЫКЛ'}`; };
 });
